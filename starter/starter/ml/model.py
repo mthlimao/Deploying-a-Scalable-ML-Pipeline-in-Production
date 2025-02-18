41f55c73f965fe9d2ee6773adf43ac813f028132
Matheus Scramignon <matheus.mignon@gmail.com>
2025-02-18 18:44:33 -0300

    Add first chunks of code

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -1,4 +1,5 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
+from sklearn.ensemble import RandomForestClassifier
 
 
 # Optional: implement hyperparameter tuning.
@@ -18,7 +19,8 @@ def train_model(X_train, y_train):
         Trained machine learning model.
     """
 
-    pass
+    rf_model = RandomForestClassifier()
+    rf_model.fit(X_train, y_train)
 
 
 def compute_model_metrics(y, preds):
@@ -57,4 +59,6 @@ def inference(model, X):
     preds : np.array
         Predictions from the model.
     """
-    pass
+    preds = model.predict(X)
+
+    return preds
--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -1,10 +1,13 @@
 # Script to train machine learning model.
-
+import numpy as np
+import pandas as pd
 from sklearn.model_selection import train_test_split
-
-# Add the necessary imports for the starter code.
+from data import process_data
+from model import train_model
+from constants import DATA_PATH
 
 # Add code to load in the data.
+data = pd.read_csv(DATA_PATH / 'census.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -19,10 +22,15 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
 X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
 # Proces the test data with the process_data function.
+X_test, y_test, _, _ = process_data(
+    test, categorical_features=cat_features, encoder=encoder, lb=lb, training=False
+)
 
 # Train and save a model.
+train_model(X_train, y_train)
